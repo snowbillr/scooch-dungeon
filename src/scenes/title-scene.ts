f32e2f5a74be62ad9f7c6d47fe1a8fc89da3adab
Bill Reed <billnreed@gmail.com>
2020-04-24 21:31:47 -0400

    title scene animations

--- src/scenes/title-scene.ts
+++ src/scenes/title-scene.ts
@@ -1,3 +1,5 @@
+import { Viewport } from "../constants/viewport";
+
 export class TitleScene extends Phaser.Scene {
   constructor() {
     super({ key: 'title' });
@@ -6,13 +8,58 @@ export class TitleScene extends Phaser.Scene {
   create() {
     this.cameras.main.setBackgroundColor(0x25131a);
 
-    this.add.image(this.cameras.main.centerX, 100, 'logo')
+    const logo = this.add.image(-300, 100, 'logo')
       .setScale(0.6);
 
-    const playText = this.add.bitmapText(this.cameras.main.centerX, 300, 'matchup-64', 'PLAY').setOrigin(0.5);
-    playText.setInteractive();
-    playText.on(Phaser.Input.Events.POINTER_DOWN, () => {
-      this.scene.start('dungeon', { levelNumber: 1 });
-    })
+    const playButton = this.addButton(this.cameras.main.centerX, Viewport.HEIGHT + 100, 'PLAY', () => this.scene.start('dungeon', { levelNumber: 1 }));
+
+    this.tweens.timeline({
+      tweens: [
+        {
+          targets: logo,
+          props: {
+            x: this.cameras.main.centerX
+          },
+          ease: Phaser.Math.Easing.Quadratic.Out,
+          duration: 750,
+        },
+        {
+          targets: playButton,
+          props: {
+            y: 300
+          },
+          offset: 300,
+          ease: Phaser.Math.Easing.Quadratic.Out,
+          duration: 550
+        }
+      ]
+    });
+  }
+
+  private addButton(x: number, y: number, text: string, onPress: () => void) {
+    const width = 200;
+    const height = 80;
+    const borderWidth = 10;
+    const borderHeight = 10;
+
+    const container = this.add.container(x, y)
+      .setSize(width, height)
+      .setInteractive()
+      .on(Phaser.Input.Events.POINTER_DOWN, onPress);
+
+    const buttonOuter = this.add.rectangle(0, 0, width, height, 0xDA4E38)
+      .setOrigin(0.5)
+
+    const buttonInner = this.add.rectangle(0, 0, width - borderWidth, height - borderHeight, 0xEE8D2E)
+      .setOrigin(0.5)
+
+    const playText = this.add.bitmapText(0, -5, 'matchup-64', 'PLAY')
+      .setOrigin(0.5)
+
+    container.add(buttonOuter);
+    container.add(buttonInner);
+    container.add(playText);
+
+    return container;
   }
 }
