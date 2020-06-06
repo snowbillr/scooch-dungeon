e726b8d91b23894ab79fb01627193a1770bfc5c4
Bill Reed <billnreed@gmail.com>
2020-06-06 16:44:53 -0400

    play spike animation

--- src/dungeon/objects/spikes.ts
+++ src/dungeon/objects/spikes.ts
@@ -10,5 +10,34 @@ export class Spikes extends DungeonObject {
     sprite: Phaser.GameObjects.Sprite,
   ) {
     super(scene, dungeonTile, name, sprite);
+
+    scene.time.addEvent({
+      loop: true,
+      delay: 7000,
+      startAt: 6000,
+      callback: () => this.startCycle()
+    });
+  }
+
+  private async startCycle() {
+    await this.playAnimation('spikes-peek');
+    await this.playAnimation('spikes-reveal');
+    await this.wait(2000);
+    await this.playAnimation('spikes-hide');
+  }
+
+  private async playAnimation(animationKey: string) {
+    return new Promise(resolve => {
+      this.sprite.on(`${Phaser.Animations.Events.SPRITE_ANIMATION_KEY_COMPLETE}${animationKey}`, () => {
+        resolve();
+      });
+      this.sprite.anims.play(animationKey);
+    });
+  }
+
+  private async wait(delay: number) {
+    return new Promise(resolve => {
+      this.scene.time.delayedCall(delay, resolve);
+    });
   }
 }
