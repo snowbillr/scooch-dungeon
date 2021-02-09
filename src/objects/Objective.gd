2ac3c0c9374937a09e1a2db848512d26bf3d0659
Bill Reed <billnreed@gmail.com>
2021-02-09 16:29:09 -0500

    less complicated signals

--- src/objects/Objective.gd
+++ src/objects/Objective.gd
@@ -12,18 +12,6 @@ func _ready() -> void:
 func _on_PlayerDetector_body_exited(body: Node) -> void:
     swipe_indicator.hide_indicator()
 
-func _on_PlayerDetectorLeft_body_entered(body: Node) -> void:
+func _on_PlayerDetector_body_entered(body: Node, from_direction: Vector2) -> void:
     if body == player:
-        swipe_indicator.show_for_direction(Vector2.RIGHT)
-
-func _on_PlayerDetectorRight_body_entered(body: Node) -> void:
-    if body == player:
-        swipe_indicator.show_for_direction(Vector2.LEFT)
-
-func _on_PlayerDetectorUp_body_entered(body: Node) -> void:
-    if body == player:
-        swipe_indicator.show_for_direction(Vector2.DOWN)
-
-func _on_PlayerDetectorDown_body_entered(body) -> void:
-     if body == player:
-        swipe_indicator.show_for_direction(Vector2.UP)
+        swipe_indicator.show_from_direction(from_direction)
--- src/objects/SwipeIndicator.gd
+++ src/objects/SwipeIndicator.gd
@@ -5,20 +5,20 @@ onready var down = $IndicatorDown
 onready var left = $IndicatorLeft
 onready var right = $IndicatorRight
 
-func show_for_direction(direction) -> void:
+func show_from_direction(direction) -> void:
     match direction:
         Vector2.UP:
-            up.visible = true
-            up.play()
-        Vector2.DOWN:
             down.visible = true
             down.play()
+        Vector2.DOWN:
+            up.visible = true
+            up.play()
         Vector2.LEFT:
-            left.visible = true
-            left.play()
-        Vector2.RIGHT:
             right.visible = true
             right.play()
+        Vector2.RIGHT:
+            left.visible = true
+            left.play()
 
 func hide_indicator() -> void:
     up.visible = false
