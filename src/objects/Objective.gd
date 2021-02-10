692497e2706ad6f5b0ff3a828c7849156637d51b
Bill Reed <billnreed@gmail.com>
2021-02-10 15:45:44 -0500

    detect swipe into objective

--- src/objects/Objective.gd
+++ src/objects/Objective.gd
@@ -5,13 +5,21 @@ export(NodePath) var player_path
 onready var swipe_indicator = $SwipeIndicator
 
 var player
+var touching_direction = null
 
 func _ready() -> void:
     player = get_node(player_path)
-
-func _on_PlayerDetector_body_exited(body: Node) -> void:
-    swipe_indicator.hide_indicator()
+    SwipeDetector.connect("swiped", self, "_on_SwipeDetector_swiped")
 
 func _on_PlayerDetector_body_entered(body: Node, from_direction: Vector2) -> void:
     if body == player:
         swipe_indicator.show_from_direction(from_direction)
+        touching_direction = from_direction
+
+func _on_PlayerDetector_body_exited(body: Node) -> void:
+    swipe_indicator.hide_indicator()
+    touching_direction = null
+
+func _on_SwipeDetector_swiped(direction) -> void:
+    if touching_direction == direction * Vector2(-1, -1):
+        print("complete!")
