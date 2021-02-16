8e0d09d49b0c15500a974b250903e946f85bc34a
Bill Reed <billnreed@gmail.com>
2021-02-16 13:20:31 -0500

    player states listen for swipes

--- src/levels/Level.gd
+++ src/levels/Level.gd
@@ -1,16 +1,4 @@
 extends Node2D
 
-# Expects children:
-# - TileMap
-# - Player
-# - SwipeDetector
-
 onready var tilemap = $TileMap
 onready var player = $Player
-
-func _ready() -> void:
-   SwipeDetector.connect("swiped", self, "_on_SwipeDetector_swiped")
-
-func _on_SwipeDetector_swiped(direction: Vector2) -> void:
-    if player.fsm.current_state.name == "idle":
-        player.fsm.transition_to("scooching", direction)
--- src/player/states/idle.gd
+++ src/player/states/idle.gd
@@ -4,3 +4,9 @@ func fizzy_enter(data) -> void:
     target.sprite.animation = "idle"
     target.sprite.playing = true
     target.movement_collision_shape.shape.extents = Vector2(16, 16)
+
+    SwipeDetector.connect("swiped", self, "_on_SwipeDetector_swiped")
+
+func _on_SwipeDetector_swiped(direction: Vector2) -> void:
+    SwipeDetector.disconnect("swiped", self, "_on_SwipeDetector_swiped")
+    fsm.transition_to("scooching", direction)
