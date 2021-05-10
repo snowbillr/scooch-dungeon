2934a2edd56712621e404321ae6eed08685d9e7c
Bill Reed <billnreed@gmail.com>
2021-05-10 10:18:05 -0400

    use editor for mainmenu rather than code to instance

--- src/Main.gd
+++ src/Main.gd
@@ -2,34 +2,32 @@ extends Node
 
 onready var world: Node2D = $World
 onready var transition: BaseTransition = $World/FadeTransition
-onready var main_menu = preload("res://src/MainMenu.tscn").instance()
+onready var main_menu = $World/MainMenu
 
 var current_level
 
 func _ready() -> void:
-    main_menu.connect("level_selected", self, "_on_MainMenu_level_selected")
-
-    world.add_child(main_menu)
+	main_menu.connect("level_selected", self, "_on_MainMenu_level_selected")
 
 func _on_MainMenu_level_selected(level_set_name) -> void:
-    current_level = load("res://src/levels/BaseLevel.tscn").instance()
-    current_level.connect("level_completed", self, "_on_Level_completed")
+	current_level = load("res://src/levels/BaseLevel.tscn").instance()
+	current_level.connect("level_completed", self, "_on_Level_completed")
 
-    transition.begin()
-    yield(transition, "at_midpoint")
+	transition.begin()
+	yield(transition, "at_midpoint")
 
-    world.add_child(current_level)
-    world.remove_child(main_menu)
+	world.add_child(current_level)
+	world.remove_child(main_menu)
 
-    transition.finish()
+	transition.finish()
 
 func _on_Level_completed() -> void:
-    transition.begin()
-    yield(transition, "at_midpoint")
+	transition.begin()
+	yield(transition, "at_midpoint")
 
-    current_level.queue_free()
-    world.remove_child(current_level)
+	current_level.queue_free()
+	world.remove_child(current_level)
 
-    world.add_child(main_menu)
+	world.add_child(main_menu)
 
-    transition.finish()
+	transition.finish()
