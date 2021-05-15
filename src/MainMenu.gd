34bb9a78700e6fb32daf11762852c893377749a4
Bill Reed <billnreed@gmail.com>
2021-05-15 15:28:42 -0400

    remove leftovers

--- src/MainMenu.gd
+++ src/MainMenu.gd
@@ -4,7 +4,3 @@ signal level_selected
 
 func _on_Button_pressed(level_identifier) -> void:
 	emit_signal("level_selected", level_identifier)
-
-
-func _on_Button2_pressed(extra_arg_0):
-	pass # Replace with function body.
