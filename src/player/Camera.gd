69052c0616cb12126470aecb6f6457e2c1b719d4
Bill Reed <billnreed@gmail.com>
2021-05-16 11:26:52 -0400

    cleanup

--- src/player/Camera.gd
+++ src/player/Camera.gd
@@ -1,13 +1,10 @@
 extends Camera2D
 
-signal transition_halfway_finished
-
 onready var AnimationPlayer = $AnimationPlayer
-onready var transition_overlay = $icon
 
 func limit_to_room(room: Room):
-    var camera_limits = room.get_limits_for_camera()
-    limit_left = camera_limits.left
-    limit_top = camera_limits.top
-    limit_right = camera_limits.right
-    limit_bottom = camera_limits.bottom
+	var camera_limits = room.get_limits_for_camera()
+	limit_left = camera_limits.left
+	limit_top = camera_limits.top
+	limit_right = camera_limits.right
+	limit_bottom = camera_limits.bottom
